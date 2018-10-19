d7d0ca6a554d5fbb71ada232e0ae00c2a7bbb513
Praveen Nerellapalli <pnerellapalli@atpco.net>
2018-10-19 10:19:32 -0400

    Initial commit

---
+++ src/main/java/com/got/quotes/gotquotesservice/GotQuotesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.got.quotes.gotquotesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotQuotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotQuotesServiceApplication.class, args);
+	}
+}
